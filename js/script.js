f73111baddd0337f7534f54ca6111123c3719bdd
tduclos <67517520+tduclos@users.noreply.github.com>
2020-07-13 12:32:44 -0400

    Set focus & 'other' text input

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 3 - Interactive Form
+******************************************/
+
+
+//set focus on 'Name' field on page load
+window.onload = function() { 
+  document.getElementById("name").focus();
+}
