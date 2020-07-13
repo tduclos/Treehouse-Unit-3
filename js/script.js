27c1eb36f7a9d2cc8c537e68829acfb2ed34cfde
tduclos <67517520+tduclos@users.noreply.github.com>
2020-07-13 12:53:40 -0400

    Job Role listener

--- js/script.js
+++ js/script.js
@@ -3,14 +3,32 @@ Treehouse Techdegree:
 FSJS project 3 - Interactive Form
 ******************************************/
 
+//Declarations
+
+const nameField = document.getElementById("name");
+const otherField = document.getElementById("other-title");
+const jobRole = document.getElementById('title');
 
 //Actions to take on page load
 window.onload = function() { 
     //set focus on 'Name' field on page load
-    document.getElementById("name").focus();
-    //Hide the 'other' text input until the user selects the 'other' text option
-    document.getElementById("other-title").style.display='none';
+    nameField.focus();
+    //Hide the 'other' field until the user selects the 'other' text option
+    otherField.style.display='none';
 }
+
+//Job Role Section
+
+//listener to show/hide the 'other' field based on the user's selected job role
+jobRole.addEventListener('change', (e) => {
+    
+   if(e.target.value ==='other'){
+       otherField.style.display='';
+   }else{
+       otherField.style.display='none';
+   }
+    
+});
 
 //T-shirt Section
 
