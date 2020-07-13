9e7e866d6d0d00212013ed33b515a44152785ed6
tduclos <67517520+tduclos@users.noreply.github.com>
2020-07-13 12:40:04 -0400

    Script structuring

--- js/script.js
+++ js/script.js
@@ -4,10 +4,18 @@ FSJS project 3 - Interactive Form
 ******************************************/
 
 
-//set focus on 'Name' field on page load
+//Actions to take on page load
 window.onload = function() { 
     //set focus on 'Name' field on page load
     document.getElementById("name").focus();
     //Hide the 'other' text input until the user selects the 'other' text option
     document.getElementById("other-title").style.display='none';
 }
+
+//T-shirt Section
+
+//Activity Section
+
+//Payment Section
+
+//Form Validation
