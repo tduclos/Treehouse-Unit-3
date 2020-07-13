2921d54e99ef8af013e5d9d11e5a6877af270b0b
tduclos <67517520+tduclos@users.noreply.github.com>
2020-07-13 12:37:40 -0400

    hide 'other-title' input on load

--- js/script.js
+++ js/script.js
@@ -6,5 +6,8 @@ FSJS project 3 - Interactive Form
 
 //set focus on 'Name' field on page load
 window.onload = function() { 
-  document.getElementById("name").focus();
+    //set focus on 'Name' field on page load
+    document.getElementById("name").focus();
+    //Hide the 'other' text input until the user selects the 'other' text option
+    document.getElementById("other-title").style.display='none';
 }
